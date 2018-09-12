421904a502a017f3d22884b97d076f8924a09cd9
Anya Yaveyn <anya.yaveyn@yandex.ru>
2018-09-12 08:03:21 +0300

    done

--- src/main/kotlin/ru/hse/spb/main.kt
+++ src/main/kotlin/ru/hse/spb/main.kt
@@ -1,13 +1,64 @@
 package ru.hse.spb
 
-fun getGreeting(): String {
-    val words = mutableListOf<String>()
-    words.add("Hello,")
-    words.add("world!")
+fun String.isClosing() = this.startsWith("/")
+fun String.isTable() = this.endsWith("table")
+fun String.isCell() = this.endsWith("td")
 
-    return words.joinToString(separator = " ")
+fun readLines(): String {
+    val input = StringBuilder()
+    var newLine = readLine()
+    while (!newLine.isNullOrBlank()) {
+        input.append(newLine)
+        newLine = readLine()
+    }
+    return input.toString()
+}
+
+fun String.preprocess() = this
+        .replace("\\s".toRegex(), "")
+        .split(">", "<").filter { it.isNotBlank() }
+
+fun countCells(input : List<String>) : List<Int> {
+    val result = mutableListOf<Int>()
+
+    fun walkCells(tokenInd : Int) : Int {
+        if (input.size <= tokenInd) return 0
+
+        val firstToken = input[tokenInd]
+        assert(firstToken.isTable() && ! firstToken.isClosing())
+
+        var currentInd = tokenInd + 1
+        var cellCount = 0
+
+        while (true) {
+            val current = input[currentInd]
+
+            if (! current.isClosing()) {
+                when {
+                    current.isCell()  -> cellCount++
+                    current.isTable() -> currentInd = walkCells(currentInd)
+                }
+            }
+            if (current.isTable() && current.isClosing()) {
+                result.add(cellCount)
+                break
+            }
+            currentInd++
+        }
+        return currentInd
+    }
+
+    walkCells(0)
+    return result
+}
+
+fun countCellsFromHtml(input: String) : List<Int> {
+    return countCells(input.preprocess())
 }
 
 fun main(args: Array<String>) {
-    println(getGreeting())
+    val counts = countCellsFromHtml(readLines())
+
+    print(counts.sorted().map { it.toString() }.joinToString(" "))
 }
+
--- src/test/kotlin/ru/hse/spb/test.kt
+++ src/test/kotlin/ru/hse/spb/test.kt
@@ -5,7 +5,36 @@ import org.junit.Test
 
 class TestSource {
     @Test
-    fun testGreeting() {
-        assertEquals("Hello, world!", getGreeting())
+    fun test1() {
+        val result = countCellsFromHtml("<table><tr><td></td></tr></table>").sorted()
+        assertEquals(listOf(1), result)
+    }
+
+    @Test
+    fun test2() {
+        val result = countCellsFromHtml("<table>\n" +
+                "<tr>\n" +
+                "<td>\n" +
+                "<table><tr><td></td></tr><tr><td></\n" +
+                "td\n" +
+                "></tr><tr\n" +
+                "><td></td></tr><tr><td></td></tr></table>\n" +
+                "</td>\n" +
+                "</tr>\n" +
+                "</table>").sorted()
+        assertEquals(listOf(1, 4), result)
+    }
+
+    @Test
+    fun test3() {
+        val result = countCellsFromHtml("<table><tr><td>\n" +
+                "<table><tr><td>\n" +
+                "<table><tr><td>\n" +
+                "<table><tr><td></td><td></td>\n" +
+                "</tr><tr><td></td></tr></table>\n" +
+                "</td></tr></table>\n" +
+                "</td></tr></table>\n" +
+                "</td></tr></table>").sorted()
+        assertEquals(listOf(1, 1, 1, 3), result)
     }
 }
